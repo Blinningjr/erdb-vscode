52a931d011d17eda183536c0dd115f1798454c7d
Niklas <nicke.l@telia.com>
2021-06-14 16:57:48 +0200

    Removed old code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,71 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
+
 import * as vscode from 'vscode';
+import { DebugAdapterTracker, DebugAdapterTrackerFactory,  } from 'vscode';
+import { exec } from 'child_process';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const tracker_factory = new RdbRsDebugAdapterTrackerFactory();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "master-thesis-vscode-debug" is now active!');
+    const descriptor_factory = new RdbRsDebugAdapterDescriptorFactory();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('master-thesis-vscode-debug.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from master-thesis-vscode-debug!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory('rdb_rs', tracker_factory));
+    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('rdb_rs', descriptor_factory));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class RdbRsDebugAdapterTrackerFactory implements DebugAdapterTrackerFactory {
+    createDebugAdapterTracker(session: vscode.DebugSession): vscode.ProviderResult<vscode.DebugAdapterTracker> {
+        console.log("Creating new debug adapter tracker");
+
+        const tracker = new RdbRsDebugAdapterTracker();
+
+        return tracker;
+    }
+}
+
+class RdbRsDebugAdapterTracker implements DebugAdapterTracker {
+    onWillReceiveMessage(message: any) {
+        console.log("Sending message to debug adapter:\n", message);
+    }
+
+    onDidSendMessage(message: any) {
+        console.log("Received message from debug adapter:\n", message);
+    }
+
+    onError(error: Error) {
+        console.log("Error in communication with debug adapter:\n", error);
+    }
+
+    onExit(code: number, signal: string) {
+        if (code) {
+            console.log("Debug Adapter exited with exit code", code);
+        } else {
+            console.log("Debug Adapter exited with signal", signal);
+        }
+    }
+}
+
+
+
+class RdbRsDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+
+
+    createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+        console.log("Session: ", session);
+        console.log("Configuration: ", session.configuration);
+
+
+        if (session.configuration.server_mode) {
+            console.log("Using existing server on port %d", session.configuration.server_port);
+            // make VS Code connect to debug server
+            return new vscode.DebugAdapterServer(session.configuration.server_port);
+        } else {
+            console.log("Using executable: ", executable);
+            return executable;
+        }
+    }
+
+    dispose() {
+        // stop server?
+    }
+}
