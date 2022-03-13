c31a2a1435fee8c4bfb8f696465a0cd4082230e2
Blinningjr <nicke.l@telia.comNO_SPAM>
2022-03-13 17:31:16 +0100

    Extension now starts ERDB server and closes it

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 'use strict';
 
+import * as child_process from 'child_process';
 import * as vscode from 'vscode';
 import { DebugAdapterTracker, DebugAdapterTrackerFactory,  } from 'vscode';
-import { exec } from 'child_process';
+import { ChildProcessWithoutNullStreams, exec } from 'child_process';
+import { disconnect } from 'process';
 
 export function activate(context: vscode.ExtensionContext) {
     const tracker_factory = new RdbRsDebugAdapterTrackerFactory();
-
     const descriptor_factory = new RdbRsDebugAdapterDescriptorFactory();
 
     context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory('rdb_rs', tracker_factory));
@@ -49,23 +50,81 @@ class RdbRsDebugAdapterTracker implements DebugAdapterTracker {
 
 class RdbRsDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
+    erdb_process?: ChildProcessWithoutNullStreams;
+    erdb_process_exited: boolean = true;
 
     createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
         console.log("Session: ", session);
         console.log("Configuration: ", session.configuration);
+        console.log("executable: ", executable);
 
+        if (!session.configuration.server_mode && this.erdb_process == undefined && this.erdb_process_exited) {
+            // Start ERDB server
+            this.erdb_process = child_process.spawn(
+                executable?.command == undefined ? "" : executable?.command,
+                executable?.args,
+                executable?.options,
+            );
+            this.erdb_process_exited = false;
 
-        if (session.configuration.server_mode) {
+            // Kill ERDB process if there is some error.
+            this.erdb_process.on('error', (exit) => {
+                if (!this.erdb_process_exited) {
+                    this.erdb_process_exited = true;
+                    this.erdb_process?.kill;
+                }
+            });
+
+            this.erdb_process.on("close", (close) => {
+                this.erdb_process_exited = true;
+                this.erdb_process?.kill;
+            });
+
+            // Kill ERDB process if parent process disconnects from ERDB porcess.
+            this.erdb_process.on('disconnect', () => {
+                this.erdb_process_exited = true;
+                this.erdb_process?.kill;
+            });
+
+            //Create output channel
+            let erdbChannel = vscode.window.createOutputChannel("erdb");
+
+            // Log that the ERDB process is stopped.
+            this.erdb_process.on('exit', (exit) => {
+                erdbChannel.appendLine("erdb server stopped");
+            });
+
+            // Log ERDB server stdout
+            this.erdb_process.stdout.on('data', (data) => {
+                erdbChannel.appendLine(data);
+            });
+
+            // Log ERDB server stderr
+            this.erdb_process.stderr.on('data', (data) => {
+                erdbChannel.appendLine(data);
+            });
+
+            (async () => { 
+                // Do something before delay
+                console.log('before delay')
+        
+                //await new Promise(f => setTimeout(f, 2000));
+        
+                // Do something after
+                console.log('after delay')
+            })();  
+               
+
+        } else {
             console.log("Using existing server on port %d", session.configuration.server_port);
             // make VS Code connect to debug server
             return new vscode.DebugAdapterServer(session.configuration.server_port);
-        } else {
-            console.log("Using executable: ", executable);
-            return executable;
-        }
+        } 
+        return new vscode.DebugAdapterServer(session.configuration.server_port);
     }
 
     dispose() {
-        // stop server?
+        this.erdb_process_exited = true;
+        this.erdb_process?.kill();
     }
 }
